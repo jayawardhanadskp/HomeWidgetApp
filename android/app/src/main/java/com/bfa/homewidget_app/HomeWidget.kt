504717e2c56da3fdf521e60d8729b1d67703452c
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-05 13:34:00 +0530

    Home Widget adroid setup done

--- android/app/src/main/java/com/bfa/homewidget_app/HomeWidget.kt
+++ android/app/src/main/java/com/bfa/homewidget_app/HomeWidget.kt
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin // Flutter plugin
+
 
 /**
  * Implementation of App Widget functionality.
@@ -12,7 +14,15 @@ class HomeWidget : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+
+            // Retrieve data from Flutter app
+            val widgetData = HomeWidgetPlugin.getData(context)
+
+            val views = RemoteViews(context.packageName, R.layout.home_widget).apply  {
+                val title = widgetData.getString("headline_title", null)
+                setTextViewText(R.id.title, title ?: "not set") // Update widget title
+            }
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
@@ -29,7 +39,7 @@ internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManage
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.home_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.title, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
